54dce0e3a3e212b01c7c3a443a67a49e3571f5f2
DNGRYL <dngryl@gmail.com>
2025-12-24 11:12:16 +0900

    Add files via upload

---
+++ VandingMachine/src/com/simple/inout/VmInOut1.java
@@ -0,0 +1,85 @@
+package com.simple.inout;
+
+import java.util.Scanner;
+
+public class VmInOut1 {
+
+	public static void main(String[] args) {
+		// 1. 자판기 메뉴 준비
+		// 밀크커피 (300원)
+
+		// 프로그램에 관련된 입출력 변수 준비
+		int inCoin = 0;
+		String milkCoffee = "밀크커피";
+		int milkPrice = 300;
+		boolean milkOk = false;
+		int menu;
+		int vmCoin = 0; // "0"을 넣은 이유는 들어온 돈을 누적해야하기 때문에.
+		String coinRepeat = "n";
+		String quit = "";
+
+		Scanner kbd = new Scanner(System.in);
+		do {
+		// 메뉴 출력
+		System.out.printf("%s (%d원) %s\n", milkCoffee, milkPrice, milkOk ? "o" : "x"); // 삼항 연산자
+
+		// 동전 입력
+		do {
+			System.out.print("동전 입력: ");
+			inCoin += kbd.nextInt();
+			kbd.nextLine();
+
+			// 입력한 동전 출력
+			System.out.println("잔액: " + inCoin);
+			System.out.println("동전 입력 계속 하나요? y/n");
+			coinRepeat = kbd.nextLine();
+
+		} while (coinRepeat.equals("y"));
+
+		// 밀크 커피 판매 가능상태로 변환 처리
+		// 입력한 동전이 300원 이상이면 판매가능 선택 "O"
+		if (inCoin >= milkPrice) {
+			milkOk = true;
+		}
+
+		// 밀크커피 판매 처리
+		// 커피 선택하기
+		// 판매에 따른 처리
+		System.out.printf("%s (%d원) %s\n", milkCoffee, milkPrice, milkOk ? "o" : "x");
+
+		if (milkOk) {
+
+			System.out.print("메뉴 선택: 밀크커피: 1");
+			menu = kbd.nextInt();
+
+			switch (menu) {
+			case 1:
+				// 자판기가 판매금액 회수
+				vmCoin += milkPrice;
+				inCoin -= milkPrice;
+				System.out.println(milkCoffee + "나왔습니다.");
+			}
+
+			// 밀크커피 상태 체크
+			if (inCoin >= milkPrice) {
+				milkOk = true;
+			} else {
+				milkOk = false;
+			}
+
+			System.out.printf("%s (%d원) %s\n", milkCoffee, milkPrice, milkOk ? "o" : "x", inCoin);
+			System.out.println("잔액: " + inCoin);
+		}
+		
+		kbd.nextLine(); // 키보드 버퍼에 남아있는 문자열 처리
+		
+		System.out.println("프로그램 종료: quit 입력");
+		quit = kbd.nextLine();
+		
+		}while(! quit.equals("quit"));
+		
+		System.out.println("프로그램 종료");
+		
+	}
+	
+}
